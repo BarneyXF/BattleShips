a18a52b3dd28250257a290877f68ec48933ad1b6
Barney <deadxdbarney@gmail.com>
2017-03-11 15:38:24 +0300

    New file's hierarchy, deleted first " Project" project, addedd project for graphics(please rename it), renamed and separated "ConsoleApplication1" to "Logic" and "Console" parts.

---
+++ Headers/AI/AI.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../Logic/MainLogic.h"
+
+enum sideToShoot
+{
+	left = 0,
+	right = 1,
+	top = 2,
+	bottom = 3
+};
+
+void TurnOfAI(SeaCell(*playersField)[11][11], Player(*playersPointer), DamagedShipToBeDestroedByAI *shipToAttack,
+			  SeaCell(*enemyField)[11][11]);
+void PrintShotInfoForPlayer(int x, int y, ShotResult result, SeaCell(*playersField)[11][11],
+							SeaCell(*enemyField)[11][11]);
---
+++ Headers/AI/Tactics.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../Logic/MainLogic.h"
+
+void ChoosingTactic(int *x, int *y, DamagedShipToBeDestroedByAI *shipToAttack, SeaCell(*playersField)[11][11]);
+
+void ChooseRandomSquare(int(*x), int(*y), SeaCell(*playersField)[11][11]);
+
+void TryDestroyingSecondDeck(int *x, int *y, SeaCell(*playersField)[11][11]);
+bool TryChoosingLeft(int *x, int y, SeaCell(*playersField)[11][11]);
+bool TryChoosingRight(int *x, int y, SeaCell(*playersField)[11][11]);
+bool TryChoosingTop(int x, int *y, SeaCell(*playersField)[11][11]);
+bool TryChoosingBottom(int x, int *y, SeaCell(*playersField)[11][11]);
+
+void TacticToDestroyMoreThanTwoDecker(DamagedShipToBeDestroedByAI *shipToAttack, SeaCell(*playersField)[11][11]);
+void HorizontalShot(DamagedShipToBeDestroedByAI *shipToAttack, SeaCell(*playersField)[11][11]);
+void VerticalShot(DamagedShipToBeDestroedByAI *shipToAttack, SeaCell(*playersField)[11][11]);
+void CheckLeftShotPossibility(DamagedShipToBeDestroedByAI *shipToAttack, SeaCell(*playersField)[11][11]);
+void CheckRightShotPossibility(DamagedShipToBeDestroedByAI *shipToAttack, SeaCell(*playersField)[11][11]);
+void CheckTopShotPossibility(DamagedShipToBeDestroedByAI *shipToAttack, SeaCell(*playersField)[11][11]);
+void CheckBottomShotPossibility(DamagedShipToBeDestroedByAI *shipToAttack, SeaCell(*playersField)[11][11]);
